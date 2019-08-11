59e742fe4c3045543442880fef2c25abfb416d49
git <asyarb@hawaii.edu>
2019-08-10 20:00:13 -1000

    fix: def file

--- use-intersection-observer.d.ts
+++ use-intersection-observer.d.ts
@@ -1,42 +1,51 @@
 /**
  * React `ref` object.
- * @typedef ReactRefObject
- *
- * @property current - Must be an HTMLElement.
  */
-interface ReactRefObject {
-  current: HTMLElement | undefined
+declare interface ReactRefObject {
+  /**
+   * Must be an Element.
+   */
+  current: Element | undefined
 }
 
 /**
  * Intersection Observer configuratiopn options
- * @typedef IntersectionObserverOptions
- *
- * @property {number} threshold - Number from 0 to 1 of the percent the element needs to intersect to be considered visible. Can provide array of thresholds.
- * @property {Boolean} triggerOnce - If true, check for intersection only once. Will disconnect the IntersectionObserver instance after intersection.
- * @property {HTMLElement} root - Element that is used as the viewport for checking visibility of the provided ref.
- * @property {string} rootMargin - Margin around the root. Can have values similar to the CSS margin property.
  */
-interface IntersectionObserverOptions {
+declare interface IntersectionObserverOptions {
+  /**
+   * Number from 0 to 1 of the percent the element needs to intersect to be considered visible. Can provide array of thresholds.
+   */
   threshold: number
+
+  /**
+   * If true, check for intersection only once. Will disconnect the IntersectionObserver instance after intersection.
+   */
   triggerOnce: boolean
-  root: HTMLElement
+
+  /**
+   * Element that is used as the viewport for checking visibility of the provided ref.
+   */
+  root: Element
+
+  /**
+   * Margin around the root. Can have values similar to the CSS margin property.
+   */
   rootMargin: string
 }
 
 /**
- * Watch for the scrolling intersection of a React or HTMLElement.
+ * Watch for the scrolling intersection of a React or Element.
  *
- * @param ref - React ref or HTMLElement.
+ * @param ref - React ref or Element.
  * @param options - Configuration object to pass to the Intersection Observer.
  * @param callback - Callback to fire when the interseciton happens. Receives the Intersection Observer entry for the provided ref as an argument.
  *
- * @throws if ref is not a valid React ref or HTMLElement.
+ * @throws if ref is not a valid React ref or Element.
  *
  * @returns true if the observed element is in view, false otherwise.
  */
 export function useIntersectionObserver(
-  ref: ReactRefObject | HTMLElement,
+  ref: ReactRefObject | Element,
   options: IntersectionObserverOptions,
   callback: (entry: IntersectionObserverEntry) => void
 ): boolean
